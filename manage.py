195d8ad5881e1f4d58836bc2f81b2f526f7622ff
Eddie Janowicz <eddie.janowicz@gmail.com>
2016-04-25 21:16:04 -0700

    test

--- manage.py
+++ manage.py
@@ -16,12 +16,12 @@ manager = Manager(app)
 #manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
-
+"""
 @manager.command
 def runserver():
     #socketio.run(app, "0.0.0.0", port=5001)
     app.run()
-"""
+
 
 @manager.command
 def syncdb():
